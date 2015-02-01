c1f553216b6771c5b6fc47333ed6230235b24a76
YC <yuricurtis@gmail.com>
2015-01-31 19:16:39 -0600

    update of all files

--- js/controllers.js
+++ js/controllers.js
@@ -1,22 +1,177 @@
-/* Controllers.js */
-var contactsApp = angular.module('contactsApp', []);
+'use strict';
 
-angular.module('contactsApp.controllers', []);
-
-contactsApp.controller("AppCtrl", function ($scope) {
+angular.module('contactsApp.controllers', [])
+    .controller('AppCtrl', ['$scope', '$rootScope', '$window', '$location', function ($scope, $rootScope, $window, $location) {
+        $scope.slide = '';
+        $rootScope.back = function() {
+          $scope.slide = 'slide-right';
+          $window.history.back();
+        }
+        $rootScope.go = function(path){
+          $scope.slide = 'slide-left';
+          $location.url(path);
+        }
+    }])
 	
-	$scope.contacts = [
-            {"id": 1, "firstName": "Dustin", "lastName": "Wells", "managerId": 0, "managerName": "", "reports": 4, "title": "Chairman and CEO", "department": "Corporate", "cellPhone": "512-000-0001", "officePhone": "512-000-0001", "email": "dwells@fakemail.com", "city": "Austin, TX", "img": "dustin-wells.jpg", "twitterId": "@fakedwells", "blog": "http://headspring.com"},
-            {"id": 2, "firstName": "JT", "lastName": "McCormick", "managerId": 1, "managerName": "Dustin Wells", "reports": 2, "title": "President", "department": "Corporate", "cellPhone": "512-000-0002", "officePhone": "512-000-0002", "email": "jtmccormick@fakemail.com", "city": "Austin, TX", "img": "jt-mccormick.jpg", "twitterId": "@fakejtmccormick", "blog": "http://headspring.com"},
-            {"id": 3, "firstName": "Glenn", "lastName": "Burnside", "managerId": 1, "managerName": "Dustin Wells", "reports": 0, "title": "Exec VP, Operations", "department": "Operations", "cellPhone": "512-000-0003", "officePhone": "512-000-0003", "email": "gburnside@fakemail.com", "city": "Austin, TX", "img": "glenn-burnside.jpg", "twitterId": "@fakeglenn", "blog": "http://headspring.com"},
-            {"id": 4, "firstName": "Jimmy", "lastName": "Bogard", "managerId": 1, "managerName": "Dustin Wells", "reports": 3, "title": "Chief Technical Architect", "department": "Engineering", "cellPhone": "512-000-0004", "officePhone": "512-000-0004", "email": "jbogard@fakemail.com", "city": "Austin, TX", "img": "jimmy-bogard.jpg", "twitterId": "@fakejbogard", "blog": "http://headspring.com"},
-            {"id": 5, "firstName": "Dave", "lastName": "Valentino", "managerId": 1, "managerName": "Dustin Wells", "reports": 2, "title": "Dir. of Enterprise Sales", "department": "Sales", "cellPhone": "512-000-0005", "officePhone": "512-000-0005", "email": "dvalentino@fakemail.com", "city": "Austin, TX", "img": "dave-valentino.jpg", "twitterId": "@fakedvalentino", "blog": "http://headspring.com"},
-            {"id": 6, "firstName": "Sharon", "lastName": "Slonaker", "managerId": 4, "managerName": "Dustin Wells", "reports": 0, "title": "Dir. of Human Resources", "department": "Human Resources", "cellPhone": "512-000-0006", "officePhone": "512-000-0006", "email": "sslonaker@fakemail.com", "city": "Austin, TX", "img": "sharon-slonaker.jpg", "twitterId": "@fakesslonaker", "blog": "http://headspring.com"},
-            {"id": 7, "firstName": "Alonso", "lastName": "Robles", "managerId": 4, "managerName": "Dustin Wells", "reports": 0, "title": "Dir. of Client Services", "department": "Engineering", "cellPhone": "512-000-0007", "officePhone": "512-000-0007", "email": "arobles@fakemail.com", "city": "Austin, TX", "img": "alonso-robles.jpg", "twitterId": "@fakearobles", "blog": "http://headspring.com"},
-            {"id": 8, "firstName": "Ben", "lastName": "Heebner", "managerId": 2, "managerName": "Dustin Wells", "reports": 0, "title": "Dir. of Engineering, Austin", "department": "Engineering", "cellPhone": "512-000-0008", "officePhone": "512-000-0008", "email": "bheebner@fakemail.com", "city": "Austin, TX", "img": "ben-heebner.jpg", "twitterId": "@fakebheebner", "blog": "http://headspring.com"},
-            {"id": 9, "firstName": "Deran", "lastName": "Schilling", "managerId": 2, "managerName": "Julie Taylor", "reports": 0, "title": "Dir. of Engineering, Houston", "department": "Engineering", "cellPhone": "512-000-0009", "officePhone": "713-000-0009", "email": "dschilling@fakemail.com", "city": "Houston, TX", "img": "deran-schilling.jpg", "twitterId": "@fakedschilling", "blog": "http://headspring.com"},
-            {"id": 10, "firstName": "Cedric", "lastName": "Yao", "managerId": 5, "managerName": "Dustin Wells", "reports": 0, "title": "Dir. of Engineering, Dallas", "department": "Engineering", "cellPhone": "512-000-0010", "officePhone": "512-000-0010", "email": "cyao@fakemail.com", "city": "Dallas, TX", "img": "cedric-yao.jpg", "twitterId": "@fakecyao", "blog": "http://headspring.com"},
-            {"id": 11, "firstName": "Kristi", "lastName": "Schurk", "managerId": 4, "managerName": "Sharon Slonaker", "reports": 0, "title": "Lead Recruiter", "department": "Human Resources", "cellPhone": "713-000-0012", "officePhone": "512-000-0012", "email": "kschurk@fakemail.com", "city": "Austin, TX", "img": "kristi-schurk.jpg", "twitterId": "@fakekschurk", "blog": "http://headspring.com"},
-			{"id": 12, "firstName": "Aaron", "lastName": "Doi", "managerId": 5, "managerName": "Kristi Schurk", "reports": 0, "title": "Recruiting Coordinator", "department": "Human Resources", "cellPhone": "512-000-0011", "officePhone": "512-000-0011", "email": "adoi@fakemail.com", "city": "Austin, TX", "img": "aaron-doi.jpg", "twitterId": "@fakeadoi", "blog": "http://headspring.com"}
-        ];
-});
+	// Contact Load Controller
+    .controller('contactListCtrl', ['$scope', 'contact', function ($scope, contact) {
+        $scope.contacts = contact.query();
+    }])
+	
+	// Contact Details Controller
+    .controller('contactDetailCtrl', ['$scope', '$routeParams', 'contact', function ($scope, $routeParams, contact) {
+        $scope.contact = contact.get({contactId: $routeParams.contactId});
+    }])
+	
+	// Contact Add Controller
+    .controller('contactAddCtrl', ['$scope', '$routeParams', 'contact', function ($scope, $routeParams, contact) {
+    // Start as not visible but when button is tapped it will show as true
+		
+	$scope.addUser = function(id) {
+			if (id == 'new') {
+					$scope.visible = true;
+				} else {
+					$scope.visible = false;
+				}
+			// Create the array to hold the list of New Contacts
+			
+				$scope.contacts = [];
+				/*$scope.contacts = contact.query();*/
+			
+			// Create the function to push the data into the "contacts" array
+				$scope.newContact = function(){
+					$scope.contacts.push({
+				 firstName:$scope.contactfirstName,
+				  lastName:$scope.contactlastName,
+					 email:$scope.contactemail,
+					  date:$scope.contactdate,
+					 title:$scope.contacttitle,
+				department:$scope.contactdepartment,
+			   managerName:$scope.contactmanagerName,
+				   reports:$scope.contactreports,
+			   officePhone:$scope.contactofficePhone,
+				 cellPhone:$scope.contactcellPhone,
+					  city:$scope.contactcity,
+				   twitter:$scope.contacttwitterId,
+					  blog:$scope.contactblog
+					});
+			
+					$scope.contactfirstName = '';
+					$scope.contactlastName = '';
+					$scope.contactemail = '';
+					$scope.contactdate = '';
+					$scope.contactofficePhone = '';
+					$scope.contactcellPhone = '';
+					$scope.contacttitle = '';
+					$scope.contactdepartment = '';
+					$scope.contactmanagerName = '';
+					$scope.contactreports = '';
+					$scope.contactcity = '';
+					$scope.contacttwitterId = '';
+					$scope.contactblog = '';
+					};
+				};
+
+    }])
+	// Contact Edit Controller
+    .controller('contactEditCtrl', ['$scope', '$routeParams', 'contact', function ($scope, $routeParams, contact) {
+    // GET contacts current data
+		$scope.contact = contact.get({contactId: $routeParams.contactId});
+		
+	// Start as not visible but when button is tapped it will show as true 
+        $scope.visible = false;
+
+    // Create the array to hold the list of New Contacts
+		$scope.contacts = contact.query();
+
+    // Create the function to grab form data for the updated entry
+    	$scope.editContact = function(){
+
+        $scope.contacts.$save({
+     firstName:$scope.contactfirstName,
+      lastName:$scope.contactlastName,
+         email:$scope.contactemail,
+          date:$scope.contactdate,
+         title:$scope.contacttitle,
+    department:$scope.contactdepartment,
+   managerName:$scope.contactmanagerName,
+       reports:$scope.contactreports,
+   officePhone:$scope.contactofficePhone,
+     cellPhone:$scope.contactcellPhone,
+          city:$scope.contactcity,
+       twitter:$scope.contacttwitterId,
+          blog:$scope.contactblog
+        });
+
+        $scope.contactfirstName = '';
+        $scope.contactlastName = '';
+        $scope.contactemail = '';
+        $scope.contactdate = '';
+        $scope.contactofficePhone = '';
+        $scope.contactcellPhone = '';
+        $scope.contacttitle = '';
+        $scope.contactdepartment = '';
+        $scope.contactmanagerName = '';
+        $scope.contactreports = '';
+        $scope.contactcity = '';
+        $scope.contacttwitterId = '';
+        $scope.contactblog = '';
+
+    };
+	
+	/*$scope.editContact.$save();*/
+	
+    }])
+	
+	// Manager Report List Controller
+    .controller('ReportListCtrl', ['$scope', '$routeParams', 'Report', function ($scope, $routeParams, Report) {
+        $scope.contacts = Report.query({contactId: $routeParams.contactId});
+    }]);
+	
+	// Add Contact Controller
+	/*.controller('addCtrl', ['$scope', 'contact', function($scope, contact) { */
+
+    // Start as not visible but when button is tapped it will show as true 
+
+        /*$scope.visible = false;*/
+
+    // Create the array to hold the list of New Contacts
+
+        /*$scope.contacts = [];*/
+
+    // Create the function to push the data into the "contacts" array
+
+ /*   $scope.newContact = function(){
+
+        $scope.contacts.push({
+     firstName:$scope.contactfirstName,
+      lastName:$scope.contactlastName,
+         email:$scope.contactemail,
+          date:$scope.contactdate,
+         title:$scope.contacttitle,
+    department:$scope.contactdepartment,
+   managerName:$scope.contactmanagerName,
+       reports:$scope.contactreports,
+   officePhone:$scope.contactofficePhone,
+     cellPhone:$scope.contactcellPhone,
+          city:$scope.contactcity,
+       twitter:$scope.contacttwitterId,
+          blog:$scope.contactblog
+        });
+
+        $scope.contactfirstName = '';
+        $scope.contactlastName = '';
+        $scope.contactemail = '';
+        $scope.contactdate = '';
+        $scope.contactofficePhone = '';
+        $scope.contactcellPhone = '';
+        $scope.contacttitle = '';
+        $scope.contactdepartment = '';
+        $scope.contactmanagerName = '';
+        $scope.contactreports = '';
+        $scope.contactcity = '';
+        $scope.contacttwitterId = '';
+        $scope.contactblog = '';
+
+    };*/
+/*}]);*/
